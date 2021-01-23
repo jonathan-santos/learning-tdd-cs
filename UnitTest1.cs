2605e11d58a4321fa037f06b21870f2d5723112a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-23 20:38:21 -0300

    Project started with dotnet template nunit and a .gitignore

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace learning_tdd_cs
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
