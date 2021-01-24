9677631c199880308e3b40bae1524152123cb180
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-24 14:26:57 -0300

    Improved RomanNumberConverter and added 4 tests to TestRomanNumbers

--- src/RomanNumbers/RomanNumberConverter.cs
+++ src/RomanNumbers/RomanNumberConverter.cs
@@ -1,15 +1,39 @@
+using System.Collections.Generic;
+
 namespace RomanNumbers
 {
     public class RomanNumberConverter
     {
+        static Dictionary<char, int> _letterValues = new()
+        {
+            ['I'] = 1,
+            ['V'] = 5,
+            ['X'] = 10,
+            ['L'] = 50,
+            ['C'] = 100,
+            ['D'] = 500,
+            ['M'] = 1000,
+        };
+
         public static int Convert(string numberInRoman)
         {
-            if (numberInRoman == "I")
-                return 1;
-            else if (numberInRoman == "V")
-                return 5;
-            else
-                return 0;
+            int accumulator = 0;
+            int lastNeighbourRight = 0;
+
+            for (var i = numberInRoman.Length - 1; i >=0; i--)
+            {
+                int current = _letterValues[numberInRoman[i]];
+                int multiplier = 1;
+
+                if (current < lastNeighbourRight)
+                    multiplier = -1;
+
+                accumulator += current * multiplier;
+
+                lastNeighbourRight = current;
+            }
+
+            return accumulator;
         }
     }
 }
--- tests/TestRomanNumbers.cs
+++ tests/TestRomanNumbers.cs
@@ -17,5 +17,40 @@ namespace RomanNumbers
             var number = RomanNumberConverter.Convert("V");
             Assert.AreEqual(5, number);
         }
+
+        [Test]
+        public void NumberX()
+        {
+            var number = RomanNumberConverter.Convert("X");
+            Assert.AreEqual(10, number);
+        }
+
+        [Test]
+        public void NumberII()
+        {
+            var number = RomanNumberConverter.Convert("II");
+            Assert.AreEqual(2, number);
+        }
+
+        [Test]
+        public void NumberXXVII()
+        {
+            var number = RomanNumberConverter.Convert("XXVII");
+            Assert.AreEqual(27, number);
+        }
+
+        [Test]
+        public void NumberIX()
+        {
+            var number = RomanNumberConverter.Convert("IX");
+            Assert.AreEqual(9, number);
+        }
+
+        [Test]
+        public void NumberCXXIX()
+        {
+            var number = RomanNumberConverter.Convert("CXXIX");
+            Assert.AreEqual(129, number);
+        }
     }
 }
